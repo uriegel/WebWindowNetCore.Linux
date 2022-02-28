cffaaa7d28746e581f90798816280b7c497d2481
Uwe Riegel <uriegel@hotmail.de>
2022-02-28 08:02:24 +0100

    Settings

--- WebWindowNetCore.Linux/WebWindow.cs
+++ WebWindowNetCore.Linux/WebWindow.cs
@@ -1,11 +1,39 @@
 ﻿namespace WebWindowNetCore;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 using GtkDotNet;
+
 public class WebWindow : IWebWindow
 {
+    // TODO: Configuration: x, y width height, isMaximized
+    // TODO: Resource loading
+    // TODO: Drag and Drop internal
+    // TODO: Drag and Drop Files
+    // TODO: Events
+    
+    record Settings(int x, int y, int width, int height, bool isMaximized);
+
     public void Initialize(Configuration configuration) => this.configuration = configuration;
     public void Execute()
     {
-        var app = new Application("de.uriegel.test");
+        // TODO: to configuration
+        // TODO: Drag and Drop: Windows, Linux mit Files
+        // TODO: Commander mit Menu versteckbar
+        // TODO: In Gtk set progress from background thread
+
+
+        // TODO or -1 for not set
+        var settings = new Settings(1800, 200, 800, 600, false);
+        string jsonString = JsonSerializer.Serialize(settings);
+        var savedSettings = JsonSerializer.Deserialize<Settings>(jsonString);
+        // TODO or get from saved Settings
+
+        var organisation = "test.uriegel.de";
+        var application = "test-app";
+
+
+        var app = new Application(organisation);
         var ret = app.Run(() => 
         {
             app.EnableSynchronizationContext();
@@ -16,7 +44,15 @@ public class WebWindow : IWebWindow
             webView.Settings.EnableDeveloperExtras = true;
             app.AddWindow(window);
             window.SetTitle(configuration.Title);
-            window.SetDefaultSize(800, 600);
+            // TODO if not saved settings
+            window.SetDefaultSize(settings.width, settings.height);
+            window.Move(settings.x, settings.y);
+            if (settings.isMaximized)
+                window.Maximize();
+
+            var appData = Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), organisation, application);
+
+
             window.ShowAll();
         });
     }
