b1861babe1bbd8d5fa84f586fa9e8cf75973b120
Uwe Riegel <uriegel@hotmail.de>
2023-03-10 13:35:43 +0100

    TODO

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,21 +1,16 @@
-﻿static class Program
-{
-    // TODO Necessary?
-    [STAThread]    
-    static void Main()
-    {
-        WebView
-            .Create()
-            .InitialBounds(600, 800)
-            .Title("Commander")
-            .SaveBounds()
-            .Url($"file://{Directory.GetCurrentDirectory()}/webroot/index.html")
+﻿WebView
+    .Create()
+    .InitialBounds(600, 800)
+    .Title("Commander")
+    .SaveBounds()
+    .Url($"file://{Directory.GetCurrentDirectory()}/webroot/index.html")
 #if DEBUG            
-            .DebuggingEnabled()
+    .DebuggingEnabled()
 #endif            
-            .Build()
-            .Run("de.uriegel.Commander");    
-    }
-}
-// TODO xml doku in nuget
+    .Build()
+    .Run("de.uriegel.Commander");    
 
+// TODO xml doku in nuget
+// TODO https://webkitgtk.org/reference/webkit2gtk/2.28.2/WebKitURISchemeRequest.html
+// TODO Windows Version: https://learn.microsoft.com/en-us/dotnet/api/microsoft.web.webview2.core.corewebview2.webresourcerequested?view=webview2-dotnet-1.0.1587.40
+// TODO Favicon in Webroot, Property obsolet
